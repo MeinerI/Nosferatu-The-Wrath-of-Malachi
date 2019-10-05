767fe1dd14b5fa64ad0670470f1d438a8c7bd889
Man Sun <ramfanderu@mail.ru>
2019-10-05 15:09:41 +0300

    Update and rename fxm2obj.cs to fxm_2_x.cs

---
+++ fxm_2_x.cs
@@ -0,0 +1,310 @@
+//жжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжж
+	using System;using System.IO;using System.Linq;using System.Text;using System.Collections;using System.Collections.Generic;
+//жжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжжж
+
+sealed class fxm2obj
+{
+		static string extTextureFileName;		//	расширение текстуры
+
+		static void Main()	
+		{
+				int v1, v2, v3;				//	грани
+				float x, y, z;				//	координаты точки
+				float vn1, vn2, vn3;	//	нормали
+				float u, v;						//	развёртка
+
+				int vertex_count;			//	количество вершин
+				int faces__count;			//	количество граней
+
+				string textureFileName;		//	имя текстуры
+
+				List<string> face_list = new List<string>();
+				List<string> vert_list = new List<string>();
+				List<string> norm_list = new List<string>();
+				List<string> uvst_list = new List<string>();
+
+		//	ищем все fmx файлы в папках и подпапках
+
+				string[] allFilesName = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.fxm",  SearchOption.AllDirectories) ; 
+
+		//	для каждого fmx файла 
+
+				foreach (var fxmName in allFilesName)
+				{
+						//	открыли *.fxm файл на чтение
+
+						using (BinaryReader br = new BinaryReader(File.Open(fxmName, FileMode.Open)))
+						{
+
+								//	открыли *.x на запись 
+
+								//	TODO	//	получается слишком много файлов для саб-мешей
+								//	может быть лучше сделать для каждого из них свой каталог и переместить туда нужные текстуры?
+
+								//	Directory.CreateDirectory(Path.GetDirectoryName(fxmName) + "\\" + Path.GetFileNameWithoutExtension(fxmName));
+
+								using (StreamWriter sw = new StreamWriter(Path.GetDirectoryName(fxmName) + "/" + Path.GetFileNameWithoutExtension(fxmName) + ".x"))
+								{
+
+								//	пропускаем первые непонятные 44 байт с начала файла
+
+										for (int temp_s = 0; temp_s < 11; temp_s++ ) br.ReadSingle();		//	11 по 4
+
+								//	количество саб-мешей
+
+										float submesh__hex = br.ReadSingle();
+										byte[] byteArray = BitConverter.GetBytes(submesh__hex);
+										int submesh__count = BitConverter.ToInt32(byteArray, 0);
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+sw.WriteLine(
+@"xof 0303txt 0032
+
+Frame DXCC_ROOT {
+  FrameTransformMatrix {
+     1.0000000000000000, 0.0000000000000000, 0.0000000000000000, 0.0000000000000000,
+    0.0000000000000000, 1.0000000000000000, 0.0000000000000000, 0.0000000000000000,
+    0.0000000000000000, 0.0000000000000000, 1.0000000000000000, 0.0000000000000000,
+    0.0000000000000000, 0.0000000000000000, 0.0000000000000000, 1.0000000000000000;;
+  }
+
+  Frame RootNode {
+    FrameTransformMatrix {
+       1.0000000000000000, 0.0000000000000000, 0.0000000000000000, 0.0000000000000000,
+      0.0000000000000000, 1.0000000000000000, 0.0000000000000000, 0.0000000000000000,
+      0.0000000000000000, 0.0000000000000000, 1.0000000000000000, 0.0000000000000000,
+      0.0000000000000000, 0.0000000000000000, 0.0000000000000000, 1.0000000000000000;;
+    }
+
+    Frame Box01 {
+      FrameTransformMatrix {
+         1.0000000000000000, 0.0000000000000000, 0.0000000000000000, 0.0000000000000000,
+        0.0000000000000000, 1.0000000000000000, 0.0000000000000000, 0.0000000000000000,
+        0.0000000000000000, 0.0000000000000000, 1.0000000000000000, 0.0000000000000000,
+        0.0000000000000000, 0.0000000000000000, -1.0000000000000000, 1.0000000000000000;;
+      }
+");
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+								//	для каждой сабмеши 
+
+										for (int subm = 0; subm < submesh__count ; subm++)
+										{
+
+										//	sw.WriteLine("Mesh mShape_" + subm + " {");
+												sw.WriteLine("Mesh mShape {");
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+{										//	читаем количество букв в имени файла текстуры 
+
+												float name__lengtf = br.ReadSingle();
+												byteArray = BitConverter.GetBytes(name__lengtf);
+												int name__length = BitConverter.ToInt32 (byteArray, 0);
+
+										//	читаем имя файла текстуры 
+
+												byte[] textureFileNameHex = new byte[name__length];
+												br.Read(textureFileNameHex, 0, name__length);
+												textureFileName = System.Text.Encoding.Default.GetString(textureFileNameHex);
+}
+
+	/////////////////////////////////////////////////////////////////////////////////////////
+
+{										//	получаем имена всех текстур файлов и расширение нужного
+
+												List<string>	texture_files = Directory.GetFiles(Path.GetDirectoryName(fxmName), "*.jpg", SearchOption.AllDirectories).ToList();
+												texture_files.AddRange(Directory.GetFiles(Path.GetDirectoryName(fxmName), "*.tga", SearchOption.AllDirectories).ToList());
+
+												for (int i = 0; i < texture_files.Count; i++)
+												{
+														if (textureFileName == Path.GetFileNameWithoutExtension(texture_files[i])) 
+														{
+																extTextureFileName = Path.GetExtension(texture_files[i]);
+																break;
+														}
+												}
+													texture_files.Clear();
+}
+
+	/////////////////////////////////////////////////////////////////////////////////////////
+
+{										//	после имени пропускаем 24 байт "пустой" информации
+			
+												for (int temp_s = 0; temp_s < 6; temp_s++ ) br.ReadSingle();	//	6 по 4
+			
+										//	количество граней
+			
+												float faces__hex = br.ReadSingle();
+												byteArray = BitConverter.GetBytes(faces__hex);
+												faces__count = BitConverter.ToInt32(byteArray, 0);
+			
+										//	количество вершин
+			
+												float vertex_hex = br.ReadSingle();
+												byteArray = BitConverter.GetBytes(vertex_hex);
+												vertex_count = BitConverter.ToInt32(byteArray, 0);
+}
+
+	/////////////////////////////////////////////////////////////////////////////////////////
+
+{										//	читаем грани в виде строк f v1 v2 v3
+
+												for ( int i = 0 ; i < faces__count ; i++ )
+												{
+														v1 = br.ReadInt16();
+														v2 = br.ReadInt16();
+														v3 = br.ReadInt16();
+
+														face_list.Add("3;" + v1 + "," + v2 + "," + v3 + ";" );
+												}
+}
+
+	/////////////////////////////////////////////////////////////////////////////////////////
+
+{										//	считываем информацию по каждой вершине v vn vt
+
+												for ( int i = 0 ; i < vertex_count ; i++ )
+												{
+														x = br.ReadSingle();
+														y = br.ReadSingle();
+														z = br.ReadSingle();
+
+														vert_list.Add(x + ";" + y + ";" + z + ";");
+
+														vn1 = br.ReadSingle();
+														vn2 = br.ReadSingle();
+														vn3 = br.ReadSingle();
+
+														u = br.ReadSingle();
+														v = br.ReadSingle();
+
+														uvst_list.Add(u + ";" + v + ";");
+
+														norm_list.Add(vn1 + ";" + vn2 + ";" + vn3 + ";");
+												}
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+{
+
+//	записываем всю информацию в *.x файл
+
+sw.WriteLine(vert_list.Count + ";");
+
+for (int q = 0; q < vert_list.Count; q++) 
+{
+		sw.Write(vert_list[q]); 
+		if (q == vert_list.Count - 1 ) { sw.Write(";"); break;}
+		else sw.Write(",\n");
+}		sw.WriteLine(); 
+
+
+
+sw.WriteLine(face_list.Count + ";"); 
+
+for (int q = 0; q < face_list.Count; q++) 
+{
+		sw.Write(face_list[q]); 
+		if (q == face_list.Count - 1 ) { sw.Write(";"); break;}
+		else sw.Write(",\n");
+}		sw.WriteLine(); 
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+sw.WriteLine("MeshMaterialList {");
+sw.WriteLine("1;");
+
+sw.WriteLine(face_list.Count + ";");
+
+for (int q = 0; q < face_list.Count; q++) 
+{
+		sw.Write("0"); 
+		if (q == face_list.Count - 1 ) { sw.Write(";"); break;}
+		else sw.Write(",");
+}
+
+sw.WriteLine(@"
+Material {
+		1.0; 1.0; 1.0; 1.000000;;
+		1.000000;
+		0.000000; 0.000000; 0.000000;;
+		0.000000; 0.000000; 0.000000;;
+		TextureFilename { """ + textureFileName + extTextureFileName + @"""" + @"; }
+	}
+}
+");
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+sw.WriteLine("MeshNormals {");
+
+sw.WriteLine(norm_list.Count + ";");
+
+for (int q = 0; q < norm_list.Count; q++) 
+{
+		sw.Write(norm_list[q]); 
+		if (q == norm_list.Count - 1 ) { sw.Write(";"); break;}
+		else sw.Write(",\n");
+}		sw.WriteLine(); 
+
+
+sw.WriteLine(face_list.Count + ";"); 
+
+for (int q = 0; q < face_list.Count; q++) 
+{
+		sw.Write(face_list[q]); 
+		if (q == face_list.Count - 1 ) { sw.Write(";"); break;}
+		else sw.Write(",\n");
+}		sw.WriteLine(); 
+
+sw.WriteLine("}");			//	закрыли MeshNormals
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+sw.WriteLine("MeshTextureCoords {");
+
+sw.WriteLine(uvst_list.Count + ";");
+
+for (int q = 0; q < uvst_list.Count; q++) 
+{
+		sw.Write(uvst_list[q]); 
+		if (q == uvst_list.Count - 1 ) { sw.Write(";"); break;}
+		else sw.Write(",\n");
+}		sw.WriteLine(); 
+
+sw.WriteLine("}");			//	закрыли MeshTextureCoords
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+sw.WriteLine(@"
+}
+}
+}
+}");
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+vert_list.Clear();
+uvst_list.Clear();
+norm_list.Clear();
+face_list.Clear();
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+										}		//	проход по сабмешам
+
+								}		//	using StreamWriter	//	закрываем *.x файл на запись  //	sw.Close();  //		sw.Dispose();
+
+						}		//	using BinaryReader	//	закрываем файл на чтение
+
+				}		//	foreach (var fxmName in allFilesName)
+
+		}		//	static void Main()
+
+}		//	class Program
